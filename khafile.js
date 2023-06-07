0e70e1f65ed45076286bb9b668d894d725bf96b8
bwkam <userdev987@gmail.com>
2023-06-07 13:37:38 +0200

    html

--- khafile.js
+++
@@ -1,6 +0,0 @@
-let project = new Project('Lightning');
-project.addAssets('Assets/**');
-project.addShaders('Sources/Shaders/**');
-project.addSources('Sources');
-project.addLibrary('zui');
-resolve(project);
